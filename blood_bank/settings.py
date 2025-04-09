761b0fdf3ca4385dac697e306d12cf06b24841af
amitkbiswas90 <amitkbiswas90@gmail.com>
2025-04-10 04:39:46 +0600

    static file

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'drf_yasg',
     'django_filters',
     'rest_framework',
