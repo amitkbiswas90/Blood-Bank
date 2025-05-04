da37e1dc9cb596a136c29bedfaa57b0fde82bf56
amitkbiswas90 <amitkbiswas90@gmail.com>
2025-05-04 07:45:22 +0600

    corse header

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'drf_yasg',
+    "corsheaders",
     'django_filters',
     'rest_framework',
     'blood',
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -88,6 +90,11 @@ DATABASES = {
     }
 }
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/",
+    "http://localhost:5174/",
+]
 
 
 # Password validation
