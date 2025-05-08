a774a140cb21e602869bbd793b9135a907a6a472
amitkbiswas90 <amitkbiswas90@gmail.com>
2025-05-08 21:59:26 +0600

    frontend url add

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -159,6 +159,8 @@ REST_FRAMEWORK = {
 }
 
 DJOSER = {
+    'EMAIL_FRONTEND_PROTOCOL':config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN':config('FRONTEND_DOMAIN'),
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
